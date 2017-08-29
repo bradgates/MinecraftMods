56b2f23fd025dcc683fb117b4dadbffa404f3165
Brad Gates <gates.brad@gmail.com>
2017-08-29 01:08:13 -0400

    Initial testmod for trying things out.

---
+++ Testmod/src/main/java/com/wud/testmod/wud.java
@@ -0,0 +1,20 @@
+package com.wud.testmod;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = wud.MODID, version = wud.VERSION)
+public class wud
+{
+    public static final String MODID = "testmod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
